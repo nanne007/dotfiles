4f3bbca69069b4b21580988fa518bcb7fd3f4a5c
Cao Jiafeng <funfriendcjf@gmail.com>
2016-12-14 11:12:05 +0800

    add bundle abbr

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
-function emax
-  emacsclient -t $argv
-end
-
 set PATH $HOME/.cargo/bin $PATH
+
+set -xg EDITOR 'emacsclient -nw'
+abbr -a emax emacsclient -nw
+
+abbr -a bi bundle install
+abbr -a bu bundle update
+abbr -a be bundle exec
+abbr -a bl bundle list
+abbr -a bo bundle open
+abbr -a bcn bundle clean
