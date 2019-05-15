445e360a2d1110154b42dbf193502b2bc254dc44
caojiafeng <funfriendcjf@gmail.com>
2019-05-15 22:31:10 +0800

    fish: update fish config
    Signed-off-by: caojiafeng <funfriendcjf@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,5 @@
-# auto install fisher package manager
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-# change fisher installation prefix
-set -g fisher_path $HOME/.config/fish/fisher
-
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-
-for file in $fisher_path/conf.d/*.fish
-    builtin source $file 2> /dev/null
-end
-
-
+# set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -x PATH "/usr/local/sbin" $PATH
 if test -d $HOME/bin
     set -x PATH $HOME/bin $PATH
 end
@@ -48,8 +32,6 @@ if test (uname) = "Darwin"
     set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 end
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
 set -x _DEFAULT_PROXY "http://127.0.0.1:1087"
 alias proxy="set -x -g ALL_PROXY $_DEFAULT_PROXY; set -x -g HTTP_PROXY $_DEFAULT_PROXY; set -x -g HTTPS_PROXY $_DEFAULT_PROXY"
 alias unproxy="set -e ALL_PROXY; and set -e HTTP_PROXY; and set -e HTTPS_PROXY"
@@ -58,3 +40,20 @@ alias unproxy="set -e ALL_PROXY; and set -e HTTP_PROXY; and set -e HTTPS_PROXY"
 set -x RUSTUP_DIST_SERVER "https://mirrors.ustc.edu.cn/rust-static"
 set -x RUSTUP_UPDATE_ROOT "https://mirrors.ustc.edu.cn/rust-static/rustup"
 
+
+# auto install fisher package manager
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# change fisher installation prefix
+set -g fisher_path $HOME/.config/fish/fisher
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    source $file 2> /dev/null
+end
