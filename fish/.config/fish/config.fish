fcd504cc88147cbe45772e9fbfeaa0deaf5ac8cd
caojiafeng <funfriendcjf@gmail.com>
2020-12-16 09:55:13 +0800

    update on fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,18 +8,22 @@ abbr -a bl bundle list
 abbr -a bo bundle open
 abbr -a bcn bundle clean
 
+set -l my_user_paths $HOME/bin $HOME/.cargo/bin "/usr/local/opt/mysql@5.6/bin" "/opt/homebrew/bin" "/usr/local/sbin"
 
-if test (uname) = "Darwin"
-    set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+if test -d $HOME/go
+    set -x GOPATH $HOME/go
+    if test ! -d $GOPATH/bin
+        command mkdir -p $GOPATH/bin
+    end
+    set -x GOBIN $GOPATH/bin
+    set -p my_user_paths $GOBIN
 end
 
-set -x _DEFAULT_PROXY "http://127.0.0.1:10087"
-alias proxy="set -x -g ALL_PROXY $_DEFAULT_PROXY; set -x -g HTTP_PROXY $_DEFAULT_PROXY; set -x -g HTTPS_PROXY $_DEFAULT_PROXY"
-alias unproxy="set -e ALL_PROXY; and set -e HTTP_PROXY; and set -e HTTPS_PROXY"
-
-# see https://lug.ustc.edu.cn/wiki/mirrors/help/rust-static for more details
-set -x RUSTUP_DIST_SERVER "https://mirrors.ustc.edu.cn/rust-static"
-set -x RUSTUP_UPDATE_ROOT "https://mirrors.ustc.edu.cn/rust-static/rustup"
+for x in $my_user_paths
+    if begin not contains $x $fish_user_paths; and test -d $x; end
+        set -a --path fish_user_paths $x
+    end
+end
 
 # auto install fisher package manager
 if not functions -q fisher
@@ -38,20 +42,20 @@ for file in $fisher_path/conf.d/*.fish
     source $file 2> /dev/null
 end
 
-
-set -l my_user_paths $HOME/.cargo/bin $HOME/bin "/usr/local/opt/mysql@5.6/bin" "/usr/local/sbin" "/usr/local/bin"
-
-if test -d $HOME/go
-    set -x GOPATH $HOME/go
-    if test ! -d $GOPATH/bin
-        command mkdir -p $GOPATH/bin
-    end
-    set -x GOBIN $GOPATH/bin
-    set -p my_user_paths $GOBIN
+### java init
+if test (uname) = "Darwin"
+    set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 end
 
-for x in $my_user_paths
-    if begin not contains $x $fish_user_paths; and test -d $x; end
-        set -gxa fish_user_paths $x
-    end
+set -x _DEFAULT_PROXY "http://127.0.0.1:10087"
+alias proxy="set -x -g ALL_PROXY $_DEFAULT_PROXY; set -x -g HTTP_PROXY $_DEFAULT_PROXY; set -x -g HTTPS_PROXY $_DEFAULT_PROXY"
+alias unproxy="set -e ALL_PROXY; and set -e HTTP_PROXY; and set -e HTTPS_PROXY"
+
+# see https://lug.ustc.edu.cn/wiki/mirrors/help/rust-static for more details
+set -x RUSTUP_DIST_SERVER "https://mirrors.ustc.edu.cn/rust-static"
+set -x RUSTUP_UPDATE_ROOT "https://mirrors.ustc.edu.cn/rust-static/rustup"
+
+### starship init
+if command -sq starship
+    starship init fish | source
 end
